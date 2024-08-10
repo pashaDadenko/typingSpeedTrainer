ae8831bbb57297a979e810f6a96ff4082dc188fe
pashaDadenko <pasha061090@mail.ru>
2024-08-10 20:00:40 +0300

    4

--- src/components/TypingTrainer/TypingTrainer.tsx
+++ src/components/TypingTrainer/TypingTrainer.tsx
@@ -1,12 +1,12 @@
 import { Modal } from '../Modal/Modal';
 import { Reset } from '../Reset/Reset';
 import { FC, memo, useEffect } from 'react';
-import { TextDisplay } from '../Text/TextDisplay';
 import { TextInput } from '../TextInput/TextInput';
 import { endTyping, startTyping } from '../../redux/typingSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 import styles from './TypingTrainer.module.scss';
+import { TextDisplay } from '../TextDisplay/TextDisplay';
 
 export const TypingTrainer: FC = memo(() => {
 	const dispatch = useAppDispatch();
